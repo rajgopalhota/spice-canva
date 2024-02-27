ee316af802b9ec64c704adbc853719426279935e
rakesh1259 <2100032231@kluniversity.in>
2024-02-27 16:13:59 +0530

    chg

--- src/pages/Recipies.jsx
+++ src/pages/Recipies.jsx
@@ -1,26 +1,25 @@
 import React from "react";
 import RecipeCard from "../components/RecipeCard"; // Assuming RecipeCard component is in the same directory
-import recipesData from '../data/recepies.json'; // Assuming you have a JSON file with recipes data
+import recipesData from "../data/recepies.json"; // Assuming you have a JSON file with recipes data
 
 const Recipes = () => {
-
   return (
     <div className="recipeBox">
-    <div className="recipes-container">
-      {recipesData.recipes.map(recipe => (
-        <RecipeCard
-          key={recipe._id}
-          id = {recipe._id}
-          title={recipe.title}
-          image={recipe.image}
-          veg={recipe.veg}
-          time={recipe.time}
-          ingredients={recipe.ingredients}
-          servings={recipe.servings}
-          description={recipe.description}
-        />
-      ))}
-    </div>
+      <div className="recipes-container">
+        {recipesData.recipes.map((recipe) => (
+          <RecipeCard
+            key={recipe._id}
+            id={recipe._id}
+            title={recipe.title}
+            image={recipe.image}
+            veg={recipe.veg}
+            time={recipe.time}
+            ingredients={recipe.ingredients}
+            servings={recipe.servings}
+            description={recipe.description}
+          />
+        ))}
+      </div>
     </div>
   );
 };
